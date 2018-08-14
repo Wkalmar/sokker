cdb70a001b94867a2173592cc4d3b7e1ff1c6fb1
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-08-14 14:11:04 +0300

    Deploy

--- src/components/parts/Header.component.js
+++ src/components/parts/Header.component.js
@@ -4,6 +4,9 @@ import LoginSVG from "svg/login.svg";
 import LogoutSVG from "svg/logout.svg";
 import NeuralSVG from "svg/neural.svg";
 import HomeSVG from "svg/home.svg";
+import LanguageSVG from "svg/language.svg";
+import ProfileSVG from "svg/profile.svg";
+import HelpSVG from "svg/help.svg";
 // Styles
 import "styles/header.css";
 // Store
@@ -19,10 +22,15 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div className="header">
-				<ul className="header_menu">
-					{ store.authorizedUser && <Link to="/"><HomeSVG /></Link> }
-					{ !store.authorizedUser && <Link to="/login"><LoginSVG /></Link> }
+				<ul className="header_menu" style={{ width: '100%' }}>
+					<Link to="/"><HomeSVG /></Link>
 					{ store.authorizedUser && <Link to="/network"><NeuralSVG /></Link> }
+				</ul>
+				<ul className="header_menu">
+					<p><LanguageSVG /></p>
+					<Link to="/help"><HelpSVG /></Link>
+					{ store.authorizedUser && <Link to="/profile"><ProfileSVG /></Link> }
+					{ !store.authorizedUser && <Link to="/login"><LoginSVG /></Link> }
 					{ store.authorizedUser && <a onClick={ store.logOut }><LogoutSVG /></a> }
 				</ul>
 			</div>
