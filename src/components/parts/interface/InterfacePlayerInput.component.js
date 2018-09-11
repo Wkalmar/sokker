3bdf893549b80eae69d5a07861c8ea2adcfae6ab
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-09-11 14:12:02 +0300

    Add logic for inputs mask

--- src/components/parts/interface/InterfacePlayerInput.component.js
+++ src/components/parts/interface/InterfacePlayerInput.component.js
@@ -5,12 +5,13 @@ import { observer } from 'mobx-react';
 
 
 export default observer(function({ output, pos, color='transparent' }) {
+	const value = output.get(pos).trim().length < 2 ? `0${output.get(pos)}` : output.get(pos);
 	return (
 		<InputMask mask="99"
 				   maskChar="0"
 				   style={{ borderBottom: `6px solid ${color}` }}
 				   alwaysShowMask={ true }
-				   value={ output.get(pos) }
+				   value={ value }
 				   onChange={ (e)=> output.set(pos, e.currentTarget.value) }
 		/>
 	);
